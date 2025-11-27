c8556141a43b18064e60d501354285b891f8d419
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-11-28 00:32:14 +0530

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/python/1929-ConcatenationofArray_20251128_003212.py
@@ -0,0 +1,9 @@
+# Last updated: 11/28/2025, 12:32:12 AM
+1class Solution(object):
+2    def getConcatenation(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: List[int]
+6        """
+7        return nums*2
+8        
