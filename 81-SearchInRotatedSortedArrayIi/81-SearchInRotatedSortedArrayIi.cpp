261a10138bd24302b2f38f1cff7eacc63bfc4aa0
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-12-02 17:52:51 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.cpp
@@ -0,0 +1,14 @@
+// Last updated: 12/2/2025, 5:52:53 PM
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        while(i<=j){
+            if(nums[i]==target || nums[j]==target){
+                return true;
+            }
+            i++,j--;
+        }
+        return false;
+    }
+};
