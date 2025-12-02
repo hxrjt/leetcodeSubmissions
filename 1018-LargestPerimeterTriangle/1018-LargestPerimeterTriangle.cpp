3fa6d34797d307ae42ad460838b9420b806e838f
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-12-02 17:50:34 +0530

    Create 1018-LargestPerimeterTriangle

---
+++ 1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.cpp
@@ -0,0 +1,15 @@
+// Last updated: 12/2/2025, 5:50:36 PM
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxPerimeter=0;
+        for(int i=2;i<nums.size();i++){
+            if(nums[i]<nums[i-1]+nums[i-2]){
+                int currPerimeter=nums[i]+nums[i-1]+nums[i-2];
+                maxPerimeter=max(maxPerimeter,currPerimeter);
+            }
+        }
+        return maxPerimeter;
+    }
+};
