59f3f4a8ceeccba6874d4360dcaaeb368816fb02
hxrjt <160017190+hxrjt@users.noreply.github.com>
2025-12-02 17:51:24 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,24 @@
+// Last updated: 12/2/2025, 5:51:26 PM
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1;i<=n;i++){
+            string temp="";
+            if(i%15==0){
+                temp="FizzBuzz";
+            }
+            else if(i%3==0){
+                temp="Fizz";
+            }
+            else if(i%5==0){
+                temp="Buzz";
+            }
+            else{
+                temp=to_string(i);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
